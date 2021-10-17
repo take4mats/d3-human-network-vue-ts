4f8ef44eee2810011dbdf06e4b9c109e5acce58c
take4mats <georgeorge.m@gmail.com>
2021-10-17 22:52:57 +0900

    migrated all.  should work but havent tried yet...

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import VueCompositionAPI from "@vue/composition-api";
+
+Vue.config.productionTip = false;
+
+Vue.use(VueCompositionAPI);
+
+new Vue({
+  router,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/views/Dev.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dev">
+    <Form msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  // reactive,
+  // computed,
+  // watch,
+  // onMounted,
+} from "@vue/composition-api";
+
+import Form from "@/components/Form.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Dev",
+  components: {
+    Form,
+  },
+});
+</script>
