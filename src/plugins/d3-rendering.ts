54e3aeab0683e526ef5f39d1fee090ae106b6552
take4mats <georgeorge.m@gmail.com>
2021-10-18 16:36:38 +0900

    updated copyright sentense

--- src/plugins/d3-rendering.ts
+++ src/plugins/d3-rendering.ts
@@ -1,5 +1,5 @@
 /*
-  Copyright 2021 @take4mats
+  Copyright 2021 Takeshi George Matsuda (https://github.com/take4mats)
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
