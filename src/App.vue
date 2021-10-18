32636b9326fb766b020df73548c49cd8fdb22fde
take4mats <georgeorge.m@gmail.com>
2021-10-18 23:59:39 +0900

    app-bar simplified

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
+    <v-app-bar app dense flat color="primary" dark>
+      <v-btn to="/" text plain>
+        <v-icon>mdi-star-david</v-icon>
+        &nbsp;
+        <pre>d3-human-network</pre>
+      </v-btn>
 
       <v-spacer></v-spacer>
 
@@ -30,18 +14,12 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
       <router-view />
-      <div>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/dev">Dev</router-link>
-      </div>
     </v-main>
   </v-app>
 </template>
