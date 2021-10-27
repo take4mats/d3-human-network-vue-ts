8bc2d8c850554cd102102b1eb7c04e80ce1650ae
take4mats <georgeorge.m@gmail.com>
2021-10-27 13:32:06 +0900

    added google-analytics (issue #9)

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import VueCompositionAPI from "@vue/composition-api";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueCompositionAPI);
+
+if (process.env.NODE_ENV === "production") {
+  Vue.use(VueGtag, {
+    config: { id: "G-0EBCZGWZWT" },
+  });
+}
 
 new Vue({
   router,
